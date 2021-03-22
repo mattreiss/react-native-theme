699b1aff0d64b8920c07b639890c86bd86abbb6d
Matt Reiss <imattreiss@gmail.com>
2021-03-21 20:50:43 -0700

    text-input

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -3,7 +3,7 @@ import Components, {
   withStyledSystem,
 } from '@mattreiss/react-native-theme'
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, TextInput } from 'react-native'
 
 const { View, Text } = Components
 
@@ -22,15 +22,35 @@ const MyImage = withStyledSystem(Image, (p) => ({
   source: p.name ? { uri: p.theme.images[p.name] } : p.source,
 }))
 
+const MyTextInput = withStyledSystem(TextInput, (p) => ({
+  placeholderTextColor:
+    p.theme.colors[p.placeholderTextColor || p.color] ||
+    p.placeholderTextColor ||
+    p.color,
+}))
+
 export default function App() {
   return (
     <ThemeProvider theme={myTheme}>
       <View flex={1} bg='bg3' alignItems='center' justifyContent='center'>
         <MyImage name='sunset' width='windowWidth' height='windowWidth' />
-        <View bg='primary' borderRadius='xl' mt='md'>
+        <View bg='primary' borderRadius='xl' mt='md' p='md'>
           <Text color='myFavColor' p='md'>
             React Native Theme
           </Text>
+          <MyTextInput
+            bg='red'
+            color='text1'
+            placeholderTextColor='negative'
+            placeholder='enter info'
+            fontSize='md'
+            p='md'
+            minWidth='xl'
+            borderRadius='md'
+            multiline={false}
+            numberOfLines={1}
+            returnKeyType='done'
+          />
         </View>
       </View>
     </ThemeProvider>
